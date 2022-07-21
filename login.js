49b608932df8febd639050da6a2d81f4ba44483c
vinayak hiwase <vinayakhiwse8@gmail.com>
2022-07-21 22:32:43 +0530

    day-2

---
+++ login.js
@@ -0,0 +1,38 @@
+let form = document.querySelector("form");
+
+let Detail=JSON.parse(localStorage.getItem("data")) || [];
+  
+ form.addEventListener("submit",function(event){
+    event.preventDefault();
+
+    if( Detail.length === 0){
+        alert("No user signup");
+        return;
+    }
+    let obj1={
+        email:form.email.value,
+        password:form.password.value,
+    }
+    let Details1=false;
+    Detail.forEach(function(el){
+        if(el.personemail === obj1.email && el.prpassword === obj1.password){
+            Details1=true;
+            localStorage.setItem("final",JSON.stringify(el));
+            alert("Succefully Log In");
+            window.location.href="nav.html"
+           
+        }
+    })
+    if(Details1===false){ 
+    alert("User Does Not Exist")
+    }
+  });
+  document.getElementById("eye").addEventListener("click",function()
+  {
+      if(document.getElementById("password").type=="password")
+      {
+          document.getElementById("password").type ="text";
+      }else{
+          document.getElementById("password").type ="password";
+      }
+  });
